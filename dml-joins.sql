98bfc0235f9fb98d97e9d28ab4513f075cb2146b
krishnamohan <krishnamohan.py@gmail.com>
2020-06-19 18:02:54 +0530

    'added'

--- dml-joins.sql
+++ dml-joins.sql
@@ -1,1 +1,45 @@
+SELECT role.name,user_new.name
+FROM role
+JOIN user_new
+ON role.id=user_new.id;
+
+
+--2
+select * from role join USER_NEW
+on role.id=USER_NEW.id;
+--3
+select USER_NEW.name
+from USER_NEW join role on role.id =USER_NEW.id;
+--4
+select USER_NEW.PHONENUMBER, USER_NEW.EMAILID, profile.BATCH, DEPARTMENT.name, profile.DESIGNATION, EXPERIENCE.COMPANY_NAME
+from USER_NEW
+inner join profile on  USER_NEW.id=profile.id 
+inner join  degree on profile.DEGREE_ID = degree.id
+inner join DEPARTMENT on degree.DEPARTMENT_ID = DEPARTMENT.id
+inner join EXPERIENCE on profile.id = EXPERIENCE.PROFILE_ID
+and user_new.name = 'PROGRAD' and experience.currentValue=3;
+
+--5
+select USER_NEW.name, SKILL.name 
+from USER_NEW
+inner join role on user_new.role_id = role.id
+inner join profile on user_new.profile_id = profile.id
+inner join SKILLS on profile.id = SKILLS.id
+where role.name = 'Alumini';
+--6
+select user_new.name, experience.company_name from user_new
+inner join role on user_new.role_id = role.id
+inner join profile on user_new.profile_id = profile.id
+inner join experience on user_new.id = experience.id
+where role.name = 'Alumini'
+order by USER_NEW.name,EXPERIENCE.COMPANY_NAME;
+--7
+select user_new.name, experience.company_name from user_new
+inner join role on user_new.role_id = role.id
+inner join profile on user_new.profile_id = profile.id
+inner join experience on user_new.id = experience.id
+where role.name = 'Alumini'
+order by user_new.name,experience.company_name;
+
+
 
--- insert-queries.sql
+++ insert-queries.sql
@@ -17,8 +17,8 @@ insert into department(id,name)values(2,'IT');
 select * from department;
 
 insert into degree(id,department_id,name)values(1,1,'EEE');
-insert into degree(id,department_id,name)values(2,2,'ECE');
-
+insert into degree(id,DEPARTMENT_ID,name)values(2,2,'ECE');
+select * from degree;
 insert into profile(id,address,batch,degree_id,designation,gender)values(1,'London','En',1,'HR','Male');
 insert into profile(id,address,batch,degree_id,designation,gender)values(2,'Chennai','For',2,'Manager','Female');
 
@@ -26,18 +26,19 @@ insert into higher_degree(id,degree_name,university_name,profile_id)values(1,'BE
 insert into higher_degree(id,degree_name,university_name,profile_id)values(2,'BE','STANFORD',2);
 
 
-insert into experience(id,company_name,currentvalue,enddate,startdate,profile_id)values(2,'CTS',1,TO_DATE('2008/11/11','YYYY/MM/DD'),TO_DATE('2019/11/11','YYYY/MM/DD'),2);
-
+insert into EXPERIENCE(id,COMPANY_NAME,CURRENTVALUE,ENDDATE,STARTDATE,PROFILE_ID)values(2,'CTS',1,TO_DATE('2008/11/11','YYYY/MM/DD'),TO_DATE('2019/11/11','YYYY/MM/DD'),2);
+select * from EXPERIENCE;
 
 insert into project(id,name,number_of_members,profile_id,short_description)values(1,'Alumini management',10,1,'for school and college');
-insert into project(id,name,number_of_members,profile_id,short_description)values(2,'Library management',11,2,'for school and college');
-
+insert into project(id,name,NUMBER_OF_MEMBERS,PROFILE_ID,SHORT_DESCRIPTION)values(2,'Library management',11,2,'for school and college');
+select * from project;
 
 insert into user_new(id,emailid,name,password,phonenumber,profile_id,role_id,username)values(1,'abcd@efgh.com','abcd','efgh',9884642005,1,1,'abcdefgh');
-insert into user_new(id,emailid,name,password,phonenumber,profile_id,role_id,username)values(2,'ijkl@mnop.com','ijkl','mnop',9845621745,2,2,'ijklmnop');
-
+insert into USER_NEW(id,EMAILID,name,password,PHONENUMBER,PROFILE_ID,ROLE_ID,USERNAME)values(2,'ijkl@mnop.com','ijkl','mnop',9845621745,2,2,'ijklmnop');
+select * from user_new;
 insert into querytable(id,content,dateof,parent_id,user_id)values(1,'Alumini management',TO_DATE('2008/11/11','YYYY/MM/DD'),1,1);
-insert into querytable(id,content,dateof,parent_id,user_id)values(2,'Library management',To_DATE('2008/11/19','YYYY/MM/DD'),2,2);
-
+insert into QUERYTABLE(id,content,DATEOF,PARENT_ID,USER_ID)values(2,'Library management',TO_DATE('2008/11/19','YYYY/MM/DD'),2,2);
+select * from querytable;
  insert into event(id,dateof,descriptionValue,invitation,name,organiser_id)values(1,TO_DATE('2008/11/11','YYYY/MM/DD'),'Alumni meet','OBA','ProGrad',1);
- insert into event(id,dateof,descriptionValue,invitation,name,organiser_id)values(2,TO_DATE('2008/11/11','YYYY/MM/DD'),'Library opening','member','FACEPrep',2);
+ insert into EVENT(id,DATEOF,DESCRIPTIONVALUE,INVITATION,name,ORGANISER_ID)values(2,TO_DATE('2008/11/11','YYYY/MM/DD'),'Library opening','member','FACEPrep',2);
+select * from event;
